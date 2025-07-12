1f7ce5609a76977857eb349a6ad38472d300c166
gschowdary08 <142251857+gschowdary08@users.noreply.github.com>
2025-07-12 12:18:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+  
+}
+resource "aws_security_group" "Jenkins" {
+
+  ingress {
+
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  ingress {
+
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    
+  }
+  
+}
+
+resource "aws_instance" "Jenkins" {
+  ami = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+  key_name = "Terraform"
+  vpc_security_group_ids = [ aws_security_group.Jenkins.id ]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install -y openjdk-17-jdk
+              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+                /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+                /etc/apt/sources.list.d/jenkins.list > /dev/null
+              sudo apt update -y
+              sudo apt install -y jenkins
+              sudo systemctl enable jenkins
+              sudo systemctl start jenkins
+              EOF
+
+  tags = {
+    
+    name = "Jenkins"
+  }
+}
 
