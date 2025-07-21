ae930ff530be4ee04fb749480b5700a0cbe9bd8a
gschowdary08 <142251857+gschowdary08@users.noreply.github.com>
2025-07-21 15:21:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,8 +114,8 @@ resource "aws_db_instance" "rds" {
   engine                  = "mysql"
   engine_version          = "8.0"
   instance_class          = "db.t3.micro"
-  username                = sanju
-  password                = Sanju936
+  username                = "sanju"
+  password                = "Sanju936@"
   allocated_storage       = 20
   db_subnet_group_name    = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids  = [aws_security_group.all_traffic.id]
